8b304a809c632f53f137bf12102558eb9ba96a01
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-03-06 15:49:25 -0800

    add view all emails page

--- router.js
+++ router.js
@@ -2,6 +2,8 @@ EmailEtc.Router.map(function() {
 
   this.resource("folders", {path: '/'}, function() {
     this.resource("folder", {path: '/:folder_id'}),
-    this.resource("newEmail")})
+    this.resource("newEmail"),
+    this.resource("emails")
+  });
 
 });
---
+++ routes/emails.js
@@ -0,0 +1,5 @@
+EmailEtc.EmailsRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.find('email');
+  }
+});
