7d1fbd37d05970b2a07916cd367761b0530ba885
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-03-06 14:22:14 -0800

    add preloaded emails

--- routes/application.js
+++ routes/application.js
@@ -9,5 +9,32 @@ EmailEtc.ApplicationRoute = Ember.Route.extend({
       id: 2,
       name: "sent"
     });
+
+    this.store.push('email', {
+      id: 1,
+      to: 'you@emailetc.com',
+      from: 'met@emailetc.com',
+      subject: "new email",
+      body: 'hey, this new email client is pretty cool.',
+      folder: 1
+    });
+
+    this.store.push('email', {
+      id: 2,
+      to: 'you@emailetc.com',
+      from: 'google@gmail.com',
+      subject: 'come back',
+      body: 'We miss you!',
+      folder: 1
+    });
+
+    this.store.push('email', {
+      id: 3,
+      to: 'you@emailetc.com',
+      from: 'yahoo@ymail.com',
+      subject: 'new offer!',
+      body: 'click here for a free pony!',
+      folder: 1
+    });
   }
 });
